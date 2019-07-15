a0e2c88d717bc08529849cbe5860a7c964f1139e
Teerasej <teerasej@gmail.com>
2019-07-15 13:24:38 +0700

    commit ครั้งแรก โปรเจคตอนนี้ มี UI สั่งเพิ่ม HttpClientModule แล้ว

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,10 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private http:HttpClient) {}
+
+  calculate(a,b){
+    console.log('ตัวตั้ง', a, 'ตัวบวก', b);
+  }
 
 }
